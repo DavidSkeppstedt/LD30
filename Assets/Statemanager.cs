5428eb3bae2f31d3da248059bfebe00fd3d7c1d9
Andreas Brommund <andreas@dixum.se>
2014-08-23 12:19:19 +0200

    Add a Statemanager and a GameStatus cs file.

---
+++ Assets/Statemanager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Statemanager : MonoBehaviour {
+
+	public enum states{
+		MENU,
+		PLAY,
+		DEAD
+	};
+
+	public static states STATE;
+
+	void Awake(){
+		STATE = states.MENU;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if(STATE == states.MENU){
+			//MENU
+		}else if(STATE == states.PLAY){
+			//PLAY		
+		}else if(STATE == states.DEAD){
+			//DEAD
+		}
+
+	}
+
+
+	//Do all the reset here
+	public static void reset(){
+		GameStatus.reset();
+	}
+}
