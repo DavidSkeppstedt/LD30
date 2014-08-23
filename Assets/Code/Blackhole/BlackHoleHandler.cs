78df0654700f4b3e00fc81569b3fa3166eccdfdc
Andreas Brommund <andreas@dixum.se>
2014-08-23 22:13:54 +0200

    Black hole and stuff

---
+++ Assets/Code/Blackhole/BlackHoleHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHoleHandler : MonoBehaviour {
+
+	private GameObject go;
+	public GameObject prefab;
+	public int amount = 10; 
+	Vector2 pos; 
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < amount; i++) {
+			addBlackHole();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void addBlackHole(){
+
+
+		pos = new Vector3(Random.Range (-2000, 2000),Random.Range (-2000, 2000),0);
+		
+		go = Instantiate (prefab, Camera.main.ScreenToWorldPoint(pos), Quaternion.identity) as GameObject;
+		go.transform.Translate(0,0,10);
+		go.transform.parent = this.gameObject.transform;
+
+
+	}
+}
---
+++ Assets/Code/Blackhole/Blackhole.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blackhole : MonoBehaviour {
+
+	public Sprite[] blackhole;
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+		sr.sprite = blackhole [Random.Range(0,blackhole.Length)];
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
